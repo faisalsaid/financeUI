66da0af04d1e19f67b07d0f996ced62ed2f55320
faisalsaid <falsdesain@gmail.com>
2023-11-21 19:31:57 +0900

    setup app js

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, StatusBar } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar animated={true} />
-      <View style={styles.headerBar}>
-        <Text style={styles.headerText}>Hi, Budy</Text>
-        <Icon name="notifications-outline" size={20} />
-        <Image style={styles.profileImage} source={{ uri: 'https://mir-s3-cdn-cf.behance.net/user/230/b84884232236245.5f8ce5f911f5d.jpg' }} />
-      </View>
-      <View style={styles.balanceContanier}>
-        <Text style={styles.balanceAmount}>$13.454,-</Text>
-        <Text style={styles.balanceText}>Available balance</Text>
-      </View>
-      <View></View>
+      <Text> Halo</Text>
     </SafeAreaView>
   );
 }
@@ -25,35 +14,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'whitesmoke',
     paddingHorizontal: 18,
-    paddingTop: 22,
-  },
-
-  // Header bar START
-  headerBar: {
-    flexDirection: 'row',
-    paddingVertical: 8,
-    gap: 16,
-    alignItems: 'center',
-  },
-  profileImage: {
-    width: 30,
-    height: 30,
-    borderRadius: 12,
-  },
-  headerText: {
-    flex: 1,
-  },
-  // Header bar END
-
-  balanceContanier: {
-    paddingVertical: 8,
-  },
-
-  balanceAmount: {
-    fontSize: 32,
-  },
-  balanceText: {
-    fontSize: 14,
-    color: 'grey',
+    paddingTop: StatusBar.currentHeight,
   },
 });
