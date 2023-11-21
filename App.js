85bf3f8d3e4461582b770574695fd355573bbcf1
faisalsaid <falsdesain@gmail.com>
2023-11-21 12:26:42 +0900

    init project

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text>Hi, Budy</Text>
+        <Image style={styles.profileImage} source={{ uri: 'https://mir-s3-cdn-cf.behance.net/user/230/b84884232236245.5f8ce5f911f5d.jpg' }} />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'plum',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  profileImage: {
+    width: 40,
+    height: 40,
+    borderRadius: 8,
   },
 });
